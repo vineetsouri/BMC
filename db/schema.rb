62ad1ed045b754129df7c8f3190c7156e6f942a9
vineetsouri <vineetsouri72@gmail.com>
2015-07-31 13:37:51 +0530

    Added name to user database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150730203630) do
+ActiveRecord::Schema.define(version: 20150731080023) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20150730203630) do
     t.string   "provider"
     t.string   "uid"
     t.string   "image"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
